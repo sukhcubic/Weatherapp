8b2e9220829d56063c158f24f0313b407a9a7f61
Sukhjinder <35240995+sukhcubic@users.noreply.github.com>
2020-08-04 10:28:01 -0500

    Update Constants.java

--- app/src/main/java/com/example/weatherapp/util/Constants.java
+++ app/src/main/java/com/example/weatherapp/util/Constants.java
@@ -1,6 +1,7 @@
 package com.example.weatherapp.util;
 
 public class Constants {
+     //Storing key, zip and other info for just for simplicity's sake
     public static final String ZIP = "78727";
     public static final String API_KEY = "FTKrgGY6YnYj3xQyqC2mflhj2Im5uHav51VcsZI0Al0";
     public static final String BASE_URL= "https://weather.ls.hereapi.com";
